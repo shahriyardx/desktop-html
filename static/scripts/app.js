d1ada8eb566700da20d4c9885fb99db5ef1602c2
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2021-09-19 19:52:03 +0600

    Good looking header for mobile

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,36 +1,36 @@
 (function(){
-    fetch('https://www.cloudflare.com/cdn-cgi/trace')
-        .then(response => response.text())
-        .then(data => {
-            data = data.trim().split('\n').reduce(function(obj, pair) {
-                pair = pair.split('=');
-                return obj[pair[0]] = pair[1], obj;
-              }, {});
+    // fetch('https://www.cloudflare.com/cdn-cgi/trace')
+    //     .then(response => response.text())
+    //     .then(data => {
+    //         data = data.trim().split('\n').reduce(function(obj, pair) {
+    //             pair = pair.split('=');
+    //             return obj[pair[0]] = pair[1], obj;
+    //           }, {});
               
-              let ip = data.ip
-              fetch(`https://ipinfo.io/${ip}/json`)
-                .then(response => response.json())
-                .then(data => {
-                    lat = data.loc.split(',')[0]
-                    lon = data.loc.split(',')[1]
+    //           let ip = data.ip
+    //           fetch(`https://ipinfo.io/${ip}/json`)
+    //             .then(response => response.json())
+    //             .then(data => {
+    //                 lat = data.loc.split(',')[0]
+    //                 lon = data.loc.split(',')[1]
 
-                    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=05a4361b638235a33d86f51b86f279c2`)
-                        .then(response => response.json())
-                        .then(data => {
-                            temp = `${parseInt(data.main.temp)}°`
-                            city = data.name
-                            condition = data.weather[0].main
+    //                 fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=05a4361b638235a33d86f51b86f279c2`)
+    //                     .then(response => response.json())
+    //                     .then(data => {
+    //                         temp = `${parseInt(data.main.temp)}°`
+    //                         city = data.name
+    //                         condition = data.weather[0].main
                             
-                            weather = document.querySelector('.weather')
+    //                         weather = document.querySelector('.weather')
 
-                            if (weather) {
-                                weather.querySelector('.location').textContent = city
-                                weather.querySelector('.temp').textContent = temp
-                                weather.querySelector('.condition').textContent = condition
-                            }
-                        })
-                })
-        })
+    //                         if (weather) {
+    //                             weather.querySelector('.location').textContent = city
+    //                             weather.querySelector('.temp').textContent = temp
+    //                             weather.querySelector('.condition').textContent = condition
+    //                         }
+    //                     })
+    //             })
+    //     })
     
     
 
@@ -67,4 +67,13 @@
         e.target.classList.toggle('active')
     })
 
+    document.querySelector('header .logo').addEventListener('click', e => {
+        document.querySelector('header .menu .close').classList.toggle('active')
+        document.querySelector('header .menu').classList.toggle('active')
+    })
+
+    document.querySelector('header .menu .close').addEventListener('click', e => {
+        document.querySelector('header .menu .close').classList.toggle('active')
+        document.querySelector('header .menu').classList.toggle('active')
+    })
 })();
